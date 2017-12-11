9abd446908ea0964ed4e54be71a35f8b167d51ab
DO3B <l.mayol@laposte.net>
2017-12-11 14:56:00 +0100

    Création de compression.c et fichier.c

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-int int main(int argc, char const *argv[]) {
-  /* code */
-  //Essai sur Windows
+#include "compression.c"
+#include "fichier.c"
+
+int main(int argc, char const *argv[]) {
+
+  ouvrir_fichier("test", "compresse");
+  char chaine1[] = "Leole";
+
+  printf("%s",concatene(chaine1,"fragile"));
   return 0;
 }
