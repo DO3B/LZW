7a2b9fdd21890b86475e3811328007d9b7f15b05
DO3B <l.mayol@laposte.net>
2017-12-17 20:58:46 +0100

    Rajout des commentaires et de l'entête

--- dictionnaire.c
+++ dictionnaire.c
@@ -1,54 +1,123 @@
+/**
+ *
+ * Polytech Marseille
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de Polytech Marseille. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */
+
+/**
+ * @author JEAN Léo <leo.jean@etu.univ-amu.fr>
+ * @author MAYOL Loïc <loic.mayol@etu.univ-amu.fr>
+ *
+ * @version 0.0.1 / 20-09-2012
+ */
+
+/**
+ * @file dictionnaire.c
+ * @brief Contient tout les fonctions relatives au fonctionnement du
+ * dictionnaire
+ */
+
+
 #include "dictionnaire.h"
 #include <stdlib.h>
 
+/** @brief Renvoie le code du noeud
+* @param noeud -> noeud dont on souhaite connaître le code
+* @return Le code du noeud en paramètre
+*/
 int code_noeud(t_ptr_noeud noeud){
   return noeud->code;
 }
 
+/** @brief Renvoie la lettre correspondant au noeud
+* @param noeud -> noeud dont on souhaite connaître la lettre
+* @return La lettre du noeud en paramètre
+*/
 char lettre_noeud(t_ptr_noeud noeud){
   return noeud->lettre;
 }
 
+/** @brief Renvoie la frère du noeud
+* @param noeud -> noeud dont on souhaite connaître le frère
+* @return Le pointeur vers le frère du noeud
+*/
 t_ptr_noeud frere_noeud(t_ptr_noeud noeud){
   return noeud->frere;
 }
 
+/** @brief Renvoie la fils du noeud
+* @param noeud -> noeud dont on souhaite connaître le fils
+* @return Le pointeur vers le fils du noeud
+*/
 t_ptr_noeud fils_noeud(t_ptr_noeud noeud){
   return noeud->fils;
 }
 
+/** @brief Renvoie la père du noeud
+* @param noeud -> noeud dont on souhaite connaître le père
+* @return Le pointeur vers le père du noeud
+*/
 t_ptr_noeud pere_noeud(t_ptr_noeud noeud){
   return noeud->pere;
 }
 
+/** @brief Modifie le code du noeud ou l'ajoute
+* @param noeud -> noeud que l'on souhaite modifier
+* @param code -> nouveau code
+* @return rien
+*/
 void modifier_code(t_ptr_noeud noeud, int code){
   noeud->code = code;
-  return;
 }
 
+/** @brief Modifie la lettre du noeud ou l'ajoute
+* @param noeud -> noeud que l'on souhaite modifier
+* @param code -> nouvelle lettre
+* @return rien
+*/
 void modifier_lettre(t_ptr_noeud noeud, char lettre){
   noeud->lettre = lettre;
-  return;
 }
 
+/** @brief Modifie la lettre du noeud ou l'ajoute
+* @param noeud -> noeud que l'on souhaite modifier
+* @param code -> nouvelle lettre
+* @return rien
+*/
+void ajouter_fils(t_ptr_noeud pere, t_ptr_noeud fils){
+  pere->fils=fils;
+}
+
+/** @brief Modifie la lettre du noeud ou l'ajoute
+* @param noeud -> noeud que l'on souhaite modifier
+* @param code -> nouvelle lettre
+* @return rien
+*/
+void ajouter_frere(t_ptr_noeud pere, t_ptr_noeud frere){
+  pere->frere=frere;
+}
+
+/** @brief Créer un noeud
+* @param code -> entier contenant le code
+* @param lettre -> lettre associée au noeud
+* @param frere -> pointeur vers le frère
+* @param fils -> pointeur vers le fils
+* @return Un nouveau noeud avec les paramètres ci-dessus
+*/
 t_ptr_noeud cree_noeud(int code,char lettre, t_ptr_noeud frere, t_ptr_noeud fils){
   t_ptr_noeud nouveau;
-  nouveau=(t_ptr_noeud)malloc(sizeof(t_noeud));
+  nouveau = (t_ptr_noeud) malloc(sizeof(t_noeud));
+
   modifier_code(nouveau, code);
   modifier_lettre(nouveau, code);
   ajouter_fils(nouveau, fils);
   ajouter_frere(nouveau, frere);
+
   return nouveau;
-}
-
-void ajouter_fils(t_ptr_noeud pere, t_ptr_noeud fils){
-  pere->fils=fils;
-  return;
-}
-
-void ajouter_frere(t_ptr_noeud pere, t_ptr_noeud frere){
-  pere->frere=frere;
-  return;
 }
 
 t_ptr_noeud initialiser_dictionnaire(){
